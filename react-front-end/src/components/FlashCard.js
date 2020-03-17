b44c3a7a453ac0b907def70b2b2b7909b110135a
Rainskyriver <lianyelich14@gmail.com>
2020-03-16 18:36:32 -0700

    Add flip zIndex logic

--- react-front-end/src/components/FlashCard.js
+++ react-front-end/src/components/FlashCard.js
@@ -53,9 +53,19 @@ export default function FlashCard(props) {
     setExpanded(!expanded);
   };
 
-  const handleFlip = (event) => {
+  const handleFlip = () => {
     set(state => !state)
+    if (expanded) {
+      setExpanded(!expanded);
+    }
   }
+  const handleZIndex = () => {
+    if (flipped) {
+      return 0;
+    } else {
+      return 1;
+    }
+  };
 
 
   // const { 
@@ -69,7 +79,7 @@ export default function FlashCard(props) {
 
   return (
     <Card className={classes.root} onClick={handleFlip}>
-      <a.div className="c front" style={{ opacity: opacity.interpolate(o => 1 - o), transform }}>
+      <a.div className="c front" style={{ zIndex: `${handleZIndex()}`, opacity: opacity.interpolate(o => 1 - o), transform }}>
         <CardContent>
           <Typography variant="body2" color="textSecondary" component="p">
             QUESTION?!
