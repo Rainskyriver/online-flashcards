15406929b0999b865024cf7ac31f118b78168030
Hoang Dinh <hoangdinh90@gmail.com>
2020-03-18 23:53:24 -0700

    css cleanup

--- react-front-end/src/components/Login.js
+++ react-front-end/src/components/Login.js
@@ -1,57 +1,90 @@
-import React, { useState } from 'react';
-import { Button, TextField } from '@material-ui/core'
-import Modal from 'react-modal';
-import axios from 'axios'
+import React, { useState } from "react";
+import { Button, TextField } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "react-modal";
+import axios from "axios";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1)
+    }
+  }
+}));
 
 const customStyles = {
-  content : {
-    top                   : '25%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)',
-    overlay:{zIndex       : '10'},
-    borderRadius          : '10px',
+  content: {
+    display: "flex",
+    top: "25%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    borderRadius: "10px"
+  },
+  overlay: {
+    zIndex: 10
   }
 };
 
 export default function Login(props) {
-  const [modalIsOpen, setIsOpen] = useState(false)
+  const [modalIsOpen, setIsOpen] = useState(false);
+
   const openModal = () => {
     setIsOpen(true);
-  }
+  };
+
   const closeModal = () => {
     setIsOpen(false);
-  }
+  };
+
   const handleLogin = () => {
     props.setLogin(true);
-  }
+  };
+
   const handleLogout = () => {
     props.setLogin(false);
-  }
-  return (
-    <div style={{zIndex: 5}}>
+  };
 
-      <Button variant={'contained'} onClick={openModal} >Login</Button>
+  const classes = useStyles();
+
+  console.log(props);
+
+  return (
+    <div style={{ zIndex: 10 }}>
+      <Button variant={"contained"} onClick={openModal}>
+        Login
+      </Button>
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         style={customStyles}
         ariaHideApp={false}
       >
-        <h2>Please Login</h2>
+        <div>
+          <h2>Please Login</h2>
+          <div className={classes.root}>
+            <TextField label="Username" />
+            <TextField
+              label="Password"
+              type="password"
+            />
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between"
+          }}
+        >
           <Button onClick={closeModal}>X</Button>
-          <form action="/api/login" method="POST">
-            <TextField
-            label="Username" 
-            />
-            <TextField
-            label="Password"
-            />
+          <form action="/api/login" method="POST" style={{ display: "flex" }}>
             <Button type="submit">Login</Button>
           </form>
+        </div>
       </Modal>
     </div>
-  )
+  );
 }
