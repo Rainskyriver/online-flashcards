2e6b10b8c76e9b3001ef7493ca8675bc1800785d
Rainskyriver <lianyelich14@gmail.com>
2020-03-18 17:01:29 -0700

    bugfix in searchroute

--- react-front-end/src/components/Routes/SearchRoute.js
+++ react-front-end/src/components/Routes/SearchRoute.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 import Deck from '../Deck';
 import axios from 'axios';
-
 export default function Search() {
+  let id = 0
   const { tag } = useParams()
   const [search, setSearch] = useState([])
   useEffect(() => {
@@ -13,7 +13,6 @@ export default function Search() {
   }, [])
   const decks = search.map((result) => {
     console.log(result);
-    let id = 0
     return (
       <div style={{padding: '50px'}} key={result.id}>
         <Deck  title={result.name} description={result.description} id={id} deck_id={result.id}/>
