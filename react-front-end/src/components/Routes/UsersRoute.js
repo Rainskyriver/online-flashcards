c35a21292a965c5391c22c77159e5c8bf6cf4167
Rainskyriver <lianyelich14@gmail.com>
2020-03-18 18:04:12 -0700

    Add /api/users/:id route and add empty comp to users/:id with css styles applied

--- react-front-end/src/components/Routes/UsersRoute.js
+++ react-front-end/src/components/Routes/UsersRoute.js
@@ -1,17 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Deck from '../Deck';
 import { useParams } from "react-router-dom";
 import axios from 'axios';
-
+import '../../styles/SearchRoute.css'
+import Empty from '../Empty'
 export default function Users() {
+  let deck_id = -1
   const { id } = useParams()
+  const [decks, setDecks] = useState([])
   useEffect(() => {
     axios.get(`/api/users/${id}`).then((res) => {
-      console.log(res.data)
+      setDecks(res.data)
     })
   }, [])
+  const decklist = decks.map((deck) => {
+    deck_id++
+    console.log(deck);
+    return (
+      <div  className='searchElement' key={deck.id}>
+        <Deck title={deck.name} description={deck.description} id={deck_id} deck_id={deck.id}/>
+      </div>
+    )
+  })
   return (
     <div style={{textAlign: 'center', height: 'auto'}}>
       <h2>{`Your decks`}</h2>
+      <div className='searchContainer' >
+        <div className='searchElement' >
+        <Empty />
+        </div>
+        {decklist}
+      </div>
     </div>  
   )
 }
