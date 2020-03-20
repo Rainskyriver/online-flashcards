4880c719e172147aba065fbd0aa22b435843b166
Hoang Dinh <hoangdinh90@gmail.com>
2020-03-20 12:36:34 -0700

    add links and pages for each game from /study/:id

--- react-front-end/src/components/Routes/StudyRoutes/TestRoute.js
+++ react-front-end/src/components/Routes/StudyRoutes/TestRoute.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useParams } from "react-router-dom";
+import { Button } from '@material-ui/core';
+import axios from 'axios';
+import '../../../styles/Game.css';
 
 export default function Test() {
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios.get(`/api/decks/${id}/test`).then((res) => {
+      console.log(res.data)
+      console.log('here')
+    })
+  }, []);
+
   return (
-    <div>
-      Test
+    <div className="game-landing-page">
+      <h2>{`TEST for deck with id: ${id}`}</h2>
+      <h1>HELLO</h1>
+      <Button>Start Test</Button>
     </div>
   )
 }
--- react-front-end/src/components/StudyDeck/StudyDeckGame.js
+++ react-front-end/src/components/StudyDeck/StudyDeckGame.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import { Link } from "react-router-dom";
 
 import GameFlashCards from "./GameFlashCards";
 import GameTest from "./GameTest";
@@ -29,18 +30,26 @@ export default function StudyDeckGame(props) {
   const MEMORYMATCH = "MEMORYMATCH";
 
   // For the onClick in the Play Button to handle the route
+
   const handlePlay = game => {
-    if (mode === FLASHCARDS) {
+    if (game === FLASHCARDS) {
       // link to FLASHCARDS game route
       console.log("does it work? in FLASHCARDS");
-    } else if (mode === TEST) {
+      return "/study/:id/original";;
+
+    } else if (game === "TEST") {
       // link to TEST game route
       console.log("does it work? in TEST");
-    } else if (mode === MEMORYMATCH) {
+      return "/study/:id/test";
+
+    } else if (game === MEMORYMATCH) {
       // link to MEMORYMATCH game route
       console.log("does it work? in MEMORYMATCH");
+      return "/study/:id/match";
+
     }
   };
+
 
   return (
     <div className="game-display">
@@ -75,16 +84,20 @@ export default function StudyDeckGame(props) {
 
         {mode === MEMORYMATCH && <GameMemoryMatch />}
       </div>
-      <Button
-        variant="contained"
-        color="primary"
-        size="large"
-        startIcon={<PlayCircleFilledIcon />}
-        onClick={() => handlePlay(mode)}
-        style={{ width: "fit-content", marginTop: "30px" }}
-      >
-        Play
-      </Button>
+      {/* <Link to="/study/3/test"> */}
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          startIcon={<PlayCircleFilledIcon />}
+          onClick={() => handlePlay(mode)}
+          style={{ width: "fit-content", marginTop: "30px" }}
+          component={Link}
+          to={handlePlay(mode)}
+        >
+          Play
+        </Button>
+      {/* </Link> */}
     </div>
   );
 }
