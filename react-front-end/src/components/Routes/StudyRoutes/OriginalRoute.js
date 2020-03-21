004fa9cb6961302c7cd5acd2afedca9b1d6d4b28
Hoang Dinh <hoangdinh90@gmail.com>
2020-03-21 16:58:23 -0700

    slide animation

--- react-front-end/src/components/Routes/StudyRoutes/OriginalRoute.js
+++ react-front-end/src/components/Routes/StudyRoutes/OriginalRoute.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { useParams } from "react-router-dom";
 import { Button, IconButton } from "@material-ui/core";
 import red from "@material-ui/core/colors/red";
@@ -6,6 +6,7 @@ import green from "@material-ui/core/colors/green";
 import blue from "@material-ui/core/colors/blue";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+import { useTransition, animated } from "react-spring";
 import axios from "axios";
 import FlashCard from "../../FlashCard";
 import "../../../styles/Game.css";
@@ -13,9 +14,8 @@ import "../../../styles/Game.css";
 export default function Original() {
   const [deck, setDeck] = useState({});
   const [cards, setCards] = useState([]);
-  const [visible, setVisible] = useState(false);
   const [start, setStart] = useState(false);
-  const [currentCard, setCurrentCard] = useState(0)
+  const [currentCard, setCurrentCard] = useState(0);
   const { id } = useParams();
 
   useEffect(() => {
@@ -38,34 +38,46 @@ export default function Original() {
   const userId = getCurrentUser("LoggedIn");
 
   const flashCards = cards.map(result => {
-    return (
-      <FlashCard
-        key={result.id}
-        question={result.front}
-        image={result.image_url}
-        hint={result.hint}
-        answer={result.back}
-        resources={result.resource}
-      />
+    return ({ style }) => (
+      <animated.div style={{ ...style }}>
+        <FlashCard
+          key={result.id}
+          question={result.front}
+          image={result.image_url}
+          hint={result.hint}
+          answer={result.back}
+          resources={result.resource}
+        />
+      </animated.div>
     );
   });
 
-
   const nextCard = () => {
     if (currentCard === flashCards.length - 1) {
-      return
+      return;
     }
     setCurrentCard(currentCard + 1);
-  }
+  };
 
   const previousCard = () => {
     if (currentCard === 0) {
-      return
+      return;
     }
     setCurrentCard(currentCard - 1);
-  }
+  };
 
+  // tests for animation card slide
+  const rightTransitions = useTransition(currentCard, p => p, {
+    from: { opacity: 0, transform: "translate3d(100%,0,0)" },
+    enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
+    leave: { opacity: 0, transform: "translate3d(-80%,0,0)" }
+  });
 
+  const leftTransitions = useTransition(currentCard, p => p, {
+    from: { opacity: 0, transform: "translate3d(-80%,0,0)" },
+    enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
+    leave: { opacity: 0, transform: "translate3d(100%,0,0)" }
+  })
 
   const startGame = () => {
     if (start === false) {
@@ -86,7 +98,12 @@ export default function Original() {
                 <ArrowBackIcon />
               </IconButton>
             </div>
-            <div className="game-box">{flashCards[currentCard]}</div>
+            <div className="game-box">
+              {leftTransitions.map(({ item, props, key }) => {
+                const Page = flashCards[item];
+                return <Page key={key} style={props} />;
+              })}
+            </div>
             <div className="next-button">
               <IconButton onClick={nextCard}>
                 <ArrowForwardIcon />
